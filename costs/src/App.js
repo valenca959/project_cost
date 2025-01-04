852138252ed60b30992fc5a9d4d9d6a7f22b0f3f
valenca959 <rodrigovenedino@gmail.com>
2025-01-03 23:36:13 -0300

    Update dependencies and add routing to App.js

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
 function App() {
   return (
-    <div className="App">
-      <p>COST</p>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link> | 
+          <Link to="/contact">Contato</Link> | 
+          <Link to="/company">Empresa</Link> | 
+          <Link to="/newproject">Novo Projeto</Link>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+      <p>Footer</p>
+    </Router>
   );
 }
 
